fce0773b11f6b8b6397bd263dbff96fc441fc179
Pieter Pas <pieter.pas@outlook.com>
2020-07-07 19:12:22 +0200

    Fix build folder ARM_NEON_2_x86_SSE

--- scripts/ci/install-neon2x86.sh
+++ scripts/ci/install-neon2x86.sh
@@ -15,7 +15,7 @@ cd /tmp
 [ -d "ARM_NEON_2_x86_SSE" ] \
  || git clone --single-branch --depth=1 --branch "$version" \
         https://github.com/intel/ARM_NEON_2_x86_SSE.git
-mkdir -p googletest/build && cd $_
+mkdir -p ARM_NEON_2_x86_SSE/build && cd $_
 # Configure
 cmake .. -DCMAKE_INSTALL_PREFIX="$prefix" -DCMAKE_BUILD_TYPE=Release
 # Build
